a63123fe84970423166ddce57a1b5eb5d3c79a29
Guide Fari <guideg6@gmail.com>
2021-09-13 12:44:32 +0200

    moar cy tests, tutorial was paused for half of these ;)

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -6,13 +6,22 @@ describe("payment", () => {
     cy.findByLabelText(/password/i).type("dude");
     cy.findByRole("checkbox", { name: /remember me/i }).check();
     cy.findByRole("button", { name: /sign in/i }).click();
-    // cy.findByRole("button", { name: /next/i }).click();
     // check account balance
-    // click on pay button
+    let oldBalance;
+    cy.get("[data-test=sidenav-user-balance]").then(($balance) => (oldBalance = $balance.text()));
+    // click on new button
+    cy.findByRole("button", { name: /new/i }).click();
     // search for user
+    cy.findByRole("textbox").type("devon becker");
+    cy.findByText(/devon becker/i).click();
     // add amount & note & click pay
+    cy.findByPlaceholderText(/amount/i).type("50");
+    cy.findByPlaceholderText(/add a note/i).type("dinner");
+    cy.findByRole("button", { name: /pay/i }).click();
     // return to transactions
+    cy.findByRole("button", { name: /return to transactions/i }).click();
     // go to personal payments
+    cy.findByRole("tab", { name: /mine/i }).click();
     // click on payment
     // verify if payment was made
     // verify if payment amount was deducted
