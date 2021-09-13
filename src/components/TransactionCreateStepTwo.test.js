6ee7524225aff338c098a014906e237a4bb01a8a
Guide Fari <guideg6@gmail.com>
2021-09-13 11:28:35 +0200

    test: combine two unit tests to intergration test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,18 +2,10 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test("on initial render, the pay button is disabled", async () => {
-  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "3" }} />);
-
-  // screen.debug();
-  // screen.getAllByRole("");
-
-  // TODO: difference between getByRole & findByRole
-  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
-});
-
 test("if an amount & note is entered, the pay button becomes enabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "3" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
   userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
