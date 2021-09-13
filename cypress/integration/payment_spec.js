341c7f559642a020b7fa8b7b65748cbe8bbbdb66
Guide Fari <guideg6@gmail.com>
2021-09-13 12:23:56 +0200

    test: login stuff done

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -1,14 +1,20 @@
-describe('payment', () => {
-    it('user can make payment', () = {
-        // login
-        // check account balance
-        // click on pay button
-        // search for user
-        // add amount & note & click pay
-        // return to transactions
-        // go to personal payments
-        // click on payment
-        // verify if payment was made
-        // verify if payment amount was deducted
-    })
-})
+describe("payment", () => {
+  it("user can make payment", () => {
+    // login
+    cy.visit("/");
+    cy.findByRole("textbox", { name: /username/i }).type("dude@example.com");
+    cy.findByLabelText(/password/i).type("dude");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
+    // cy.findByRole("button", { name: /next/i }).click();
+    // check account balance
+    // click on pay button
+    // search for user
+    // add amount & note & click pay
+    // return to transactions
+    // go to personal payments
+    // click on payment
+    // verify if payment was made
+    // verify if payment amount was deducted
+  });
+});
